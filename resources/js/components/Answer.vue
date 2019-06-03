89c72d2db5d9a2dab71bdeb00a17b96512c7caaf
Jorge Aguiniga <jorge.aguiniga94@gmail.com>
2019-06-03 20:36:36 +0000

    izitoast

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -30,27 +30,47 @@ export default {
             })
             .then(res => {
                 console.log(res.data.message);
+                this.$toast.success(res.data.message, "Success", {timeout: 3000});
                 this.editing = false;
                 this.bodyHtml = res.data.body_html
             })
             .catch(err => {
+                this.$toast.success(err.data.message, "Error", {timeout: 3000});
                 console.log("Something went wrong: " + err);
-                alert(err.response.data.message);
             });
         },
 
         destroy() {
-            if (confirm('Are you sure?')) {
-                axios.delete(this.endpoint)
-                .then(res=> {
-                    $(this.$el).fadeOut(500, ()=> {
-                        console.log(res.data.message);
+            this.$toast.question('Are you sure about that?', 'Hey', {
+            timeout: 20000,
+            close: false,
+            overlay: true,
+            displayMode: 'once',
+            id: 'question',
+            zindex: 999,
+            position: 'center',
+            buttons: [
+                ['<button><b>Yes</b></button>', (instance, toast) => {
+        
+                    axios.delete(this.endpoint)
+                    .then(res=> {
+                        $(this.$el).fadeOut(500, ()=> {
+                            this.$toast.success(res.data.message, "Success", {timeout: 3000});
+                        })
                     })
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-            }
+                    .catch(err => {
+                        console.log(err);
+                    });
+                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+        
+                }, true],
+                ['<button>No</button>', function (instance, toast) {
+        
+                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+        
+                }],
+            ],
+        });
         }
     }, 
     
